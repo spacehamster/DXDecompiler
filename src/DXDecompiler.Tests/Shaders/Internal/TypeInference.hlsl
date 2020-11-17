38d4a232c7d1955472c8b07e90070d1461aa0fc1
spacehamster <mail.spacehamster@gmail.com>
2020-11-17 21:48:56 +1100

    Added new test

---
+++ src/DXDecompiler.Tests/Shaders/Internal/TypeInference.hlsl
@@ -0,0 +1,13 @@
+// FXC disassembler will interepret 1065353216 as 1.0 for mov instructions
+int loop_max;
+float4 main( ) : SV_Target
+{
+    float4 result = 0;
+    for(int i = 1; i < loop_max; i++){
+        result += i;
+    }
+    for(int i = 1065353216; i < loop_max; i++){
+        result += i;
+    }
+    return result;
+}
