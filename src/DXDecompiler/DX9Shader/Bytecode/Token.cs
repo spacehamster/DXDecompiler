92af89df2f60514c0de228d648fd524d5015ac19
spacehamster <mail.spacehamster@gmail.com>
2021-02-22 12:45:06 +1100

    Add TODO about restructuring DX9 token and instruction classes

--- src/DXDecompiler/DX9Shader/Bytecode/Token.cs
+++ src/DXDecompiler/DX9Shader/Bytecode/Token.cs
@@ -8,6 +8,17 @@
 	public class Token
 	{
 		public Opcode Opcode { get; private set; }
+
+		// TODO: Replace all references to uint[] Data with parsed C# classes.
+		// Instructions parse data into Operands
+		// DefI and Def will need to parse Data into constant values
+		// Comment tokens will need to have a uint[] Data field added 
+		// (note: I have not found any examples of comment tokens that cannot be parsed into shader model chunks instead)
+		// Instruction operations such as GetSourceName and GetDestination will need to be moved to their respective operand class
+		// This may not be easy if those methods rely on data from instructions (need to look into this)
+		// This will allow from better handling of relative addressing and allow for easier debugging
+		// See also:
+		// https://github.com/spacehamster/DXDecompiler/pull/6#issuecomment-782958769
 		public uint[] Data { get; private set; }
 		public int Modifier { get; set; }
 		public bool Predicated { get; set; }
