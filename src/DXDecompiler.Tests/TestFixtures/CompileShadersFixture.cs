07ef5e730211b1d97dc57269514d1295f6e2f06a
spacehamster <mail.spacehamster@gmail.com>
2021-02-22 12:18:31 +1100

    Change CompileShaderFixture to compile DX9 shaders in addition to DXBC shaders

--- src/DXDecompiler.Tests/TestFixtures/CompileShadersFixture.cs
+++ src/DXDecompiler.Tests/TestFixtures/CompileShadersFixture.cs
@@ -9,12 +9,14 @@ namespace DXDecompiler.Tests.Util
 	[TestFixture]
 	public class CompileShadersFixture
 	{
-		private static string ShaderDirectory => $"{TestContext.CurrentContext.TestDirectory}/Shaders";
+		private static string DXBCShaders => $"{TestContext.CurrentContext.TestDirectory}/Shaders";
+		private static string DX9Shaders => $"{TestContext.CurrentContext.TestDirectory}/ShadersDX9";
 
 		[Test]
 		public void CompileShaders()
 		{
-			ShaderCompiler.ProcessShaders(ShaderDirectory);
+			ShaderCompiler.ProcessShaders(DXBCShaders);
+			ShaderCompiler.ProcessShaders(DX9Shaders);
 		}
 	}
 }
